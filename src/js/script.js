9e72a43996c60321e017843555dfc4d9d6c936ec
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-04-08 15:35:02 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,66 @@
-const botaoTrailer = document.querySelector(".botao-trailer");
-const botaoFecharModal = document.querySelector(".fechar-modal");
-const video = document.getElementById("video");
-const modal = document.querySelector(".modal");
-const linkDoVideo = video.src;
+'use strict';
 
-function alternarModal(){
-	modal.classList.toggle("aberto");
+
+
+/**
+ * add eventListener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
 }
 
-botaoTrailer.addEventListener("click", () => {
-	alternarModal();
-	video.setAttribute("src", linkDoVideo);
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+const circle = document.querySelector("[data-circle]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  circle.style.animation = "none";
+  document.body.classList.add("loaded");
 });
 
-botaoFecharModal.addEventListener("click", () => {
-	alternarModal();
-	video.setAttribute("src", "");
-});
+
+
+/**
+ * NAVBAR TOGGLER FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * 
+ * add active class on header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", headerActive);
